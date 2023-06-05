4235ab5a27a6291629d62b71f48e31205b34d7bf
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-06-05 14:10:23 +0530

    added structure

---
+++ src/components/organisms/navbar/navbar.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import styles from "./navbar.module.css"
+
+
+export function Navbar(){
+    return(
+        <Fragment>
+            <div className={styles.navbar}>
+
+            </div>
+        </Fragment>
+    )
+}
---
+++ src/components/pages/heropage/heroPage.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import { Navbar } from "../../organisms/navbar/navbar";
+
+
+export function HeroPage(){
+    return (
+        <Fragment>
+            <div >
+                <Navbar/>
+
+            </div>
+
+            
+        </Fragment>
+    )
+}
