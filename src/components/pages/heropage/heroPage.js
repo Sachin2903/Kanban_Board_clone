221c4ccd5c83ffa9be83dfd6a15668a8fdedf9c4
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-06-05 15:52:07 +0530

    add more structure by sachin

--- src/components/pages/heropage/heroPage.js
+++ src/components/pages/heropage/heroPage.js
@@ -1,12 +1,28 @@
 import { Fragment } from "react";
 import { Navbar } from "../../organisms/navbar/navbar";
+import styles from "./heroPage.module.css";
+import { InitialTask } from "../../molecules/initialTask/initialTask";
 
 
 export function HeroPage(){
     return (
         <Fragment>
-            <div >
+            <div className={styles.heroPageMain}  >
                 <Navbar/>
+
+
+              
+                <InitialTask/>
+
+
+
+
+
+
+
+
+
+
 
             </div>
 
