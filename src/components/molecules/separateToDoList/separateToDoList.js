cf75fe32d747653f0fd6a256f881c4cce412d9dd
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-06-06 15:49:20 +0530

    ok sachin

--- src/components/molecules/separateToDoList/separateToDoList.js
+++ src/components/molecules/separateToDoList/separateToDoList.js
@@ -1,10 +1,17 @@
 import { Fragment } from "react";
+import {FiMove} from "react-icons/fi";
+import {HiDotsCircleHorizontal} from "react-icons/hi"
 import styles from "./seperateToDoList.module.css";
 import { subToDoList } from "./subToDoList/subToDoList";
 export function SeparateToDoList(){
     return(
         <Fragment>
             <div className={styles.TodoList}>
+
+                <input className={styles.ToDolistInput}/>
+                <HiDotsCircleHorizontal className={styles.Delete}/>
+
+                <FiMove className={styles.moveArrow}/>
 
           <subToDoList/>
 
