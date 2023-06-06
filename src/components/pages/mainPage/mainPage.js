6e39b0fcc51b4733f37f8893a2da243fed9c7cef
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-06-06 13:10:16 +0530

    added mainPage structure

--- src/components/pages/mainPage/mainPage.js
+++ src/components/pages/mainPage/mainPage.js
@@ -1,14 +1,25 @@
 import { Fragment } from "react";
 import styles from "./mainPage.module.css";
 
-export function MainPage(){
-    return(
+export function MainPage() {
+    return (
         <Fragment>
             <div className={styles.mainPageDiv}>
+                <div className={styles.mainPageDivNavBar}>
+    //khan
+                </div>
+
+
+                <div className={styles.mainPageContent}>
+                    //ap
+
+                </div>
 
 
 
             </div>
+
+
 
         </Fragment>
     )
