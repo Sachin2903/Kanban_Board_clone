240e483736c5cb63941fa8eba843846fe4ae79c2
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-06-08 16:01:24 +0530

    added dynamic routing by sachin

--- src/components/molecules/editTextBox/editTextBox.js
+++ src/components/molecules/editTextBox/editTextBox.js
@@ -1,18 +1,42 @@
-import { Fragment} from "react"
-import styles from "./editOpenBox.module.css"
-import { HiMenuAlt2 } from "react-icons/hi"
-import {ImCross} from "react-icons/im"
+import { Fragment ,useRef} from "react";
+import styles from "./editOpenBox.module.css";
+import { HiMenuAlt2 } from "react-icons/hi";
+import { ImCross } from "react-icons/im";
+import { useSelector, useDispatch } from "react-redux";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
 import { useNavigate, useParams } from "react-router-dom";
+import {websiteTodoSlice} from "../../../slice/websiteTodoSlice";
+import logoimg from "../../../image/man.png"
 
 export function EditOpenBox() {
-    const navigate=useNavigate();
-    const {param1,param2}=useParams();
+    const navigate = useNavigate();
+    const refoddes=useRef();
+    const { param1, param2 } = useParams();
+    const Des = useDispatch();
 
 
-  
 
+    const stateForDes = useSelector((state, action) => {
+        const mainData = state.toDoListData.find((e) => {
+            if(e.id===param1)
+               return true;
+               return false;
+        })
+           return mainData;          
+    })
 
+  const minData=stateForDes.list.find((e)=>{
+    if(e.idMin===param2)
+    return true;
+    return false;
+  })
+
+  function toSaveDesInSlice(){
+    Des(websiteTodoSlice.actions.addDescriptionToList({mainId:param1,minId:param2,titlefromdes:refoddes.current.value.trim()}));
+    refoddes.current.value=""
+  }
+
+ 
 
 
     function closeTextBox() {
@@ -26,19 +50,20 @@ export function EditOpenBox() {
             <div className={styles.editMainbox}>
                 <div className={styles.editMainboxContainer}>
                     <div className={styles.editMainboxContent}>
-                        <ImCross onClick={closeTextBox} className={styles.desBtnCancel}/>
-                        <p className={styles.DesTitle}>{param1}</p>
-                        <p className={styles.DesTitle}>{param2}</p>
+                        <ImCross onClick={closeTextBox} className={styles.desBtnCancel} />
+                        <p className={styles.DesTitle}>{minData.minTaskTitle}</p>
                         <HiMenuAlt2 className={styles.desMenu} />
                         <p className={styles.desText}>Description</p>
                         <AiOutlineQuestionCircle className={styles.desMenu} />
-                        <input className={styles.textArea} />
+                        <input ref={refoddes} className={styles.textArea} />
 
+                        <button onClick={toSaveDesInSlice} className={styles.desBtnSave}>Save</button>
 
-                        <button className={styles.desBtnSave}>Save</button>
-                        
                         <p className={styles.desBtnSideText}>Share feedback</p>
                         <div className={styles.bottomDesActivity}>
+                            {
+                                minData.descriptionOfList.map((e,i)=> <div className={styles.desBox} key={i*10}><img className={styles.desImg} src={logoimg} alt="loading"/><p className={styles.desp}>{e}</p></div>  )
+                            }
 
 
                         </div>
--- src/slice/websiteTodoSlice.js
+++ src/slice/websiteTodoSlice.js
@@ -11,7 +11,6 @@ export const websiteTodoSlice = createSlice({
                 list: [],
                 trashStatus: false,
             }
-
             return [...state, addedTodOListData]
         }),
         DeleteMyToDoList: ((state, action) => {
@@ -29,7 +28,6 @@ export const websiteTodoSlice = createSlice({
                 }
                 return e
             })
-
         }),
         TitleChnageOfMainToDoList: ((state, action) => {
             state.map((e) => {
@@ -38,63 +36,70 @@ export const websiteTodoSlice = createSlice({
                 }
                 return e;
             })
-
         }),
         addMinList: ((state, action) => {
-           const addMinListData = {
+            const addMinListData = {
                 idMin: uuidv4(),
                 minTaskTitle: action.payload.minTitle,
                 EditStatusOfMin: false,
+                descriptionOfList: []
             }
             state.map((e) => {
                 if (e.id === action.payload.idMin) {
-                    e.list = [...e.list,addMinListData]
-
+                    e.list = [...e.list, addMinListData]
                 }
                 return e
-
             })
         }),
-        deleteMinList:((state,action)=>{
-            state.map((e)=>{
-                 if(e.id===action.payload.mainId){
-                   const newMinList= e.list.filter((e)=>e.idMin!==action.payload.idToDelete)
-                   e.list=[...newMinList];
-
-                 }
-                 return e;
+        deleteMinList: ((state, action) => {
+            state.map((e) => {
+                if (e.id === action.payload.mainId) {
+                    const newMinList = e.list.filter((e) => e.idMin !== action.payload.idToDelete)
+                    e.list = [...newMinList];
+                }
+                return e;
             })
         }),
-        ToogleMinList:((state,action)=>{
-            state.map((e)=>{
-                if(e.id===action.payload.mainId){
-                  e.list.map((e)=>{
-                    if(e.idMin===action.payload.idToToggle){
-                        e.EditStatusOfMin=!e.EditStatusOfMin
-
-                    }
-                    return e;
-                })
+        ToogleMinList: ((state, action) => {
+            state.map((e) => {
+                if (e.id === action.payload.mainId) {
+                    e.list.map((e) => {
+                        if (e.idMin === action.payload.idToToggle) {
+                            e.EditStatusOfMin = !e.EditStatusOfMin
+                        }
+                        return e;
+                    })
                 }
                 return e;
-           })
-
+            })
         }),
-        changeMinTitle:((state,action)=>{
-
-            state.map((e)=>{
-                if(e.id===action.payload.mainId){
-                  e.list.map((e)=>{
-                    if(e.idMin===action.payload.idToChnageMinTitle){
-                        e.minTaskTitle=action.payload.changeName;
-
-                    }
-                    return e;
-                })
+        changeMinTitle: ((state, action) => {
+            state.map((e) => {
+                if (e.id === action.payload.mainId) {
+                    e.list.map((e) => {
+                        if (e.idMin === action.payload.idToChnageMinTitle) {
+                            e.minTaskTitle = action.payload.changeName;
+                        }
+                    })
                 }
                 return e;
-           })
+            })
         }),
+        addDescriptionToList: ((state, action) => {
+            state.map((e) => {
+                if (e.id === action.payload.mainId) {
+                    e.list.map((ee) => {
+                        if (ee.idMin === action.payload.minId) {
+                            const text = `${action.payload.titlefromdes}`
+                            ee.descriptionOfList = [text, ...ee.descriptionOfList];
+                        }
+                        return ee;
+                    })
+                }
+                return e;
+            })
+        }),
+
 
 
 
