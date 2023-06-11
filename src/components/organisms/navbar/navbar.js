62c5a240ec3fed97260ea938baa41f4d8693db8c
Rahulbbbb <beingbankoti@gmail.com>
2023-06-12 01:04:01 +0530

    backgroundImages

--- src/components/organisms/navbar/navbar.js
+++ src/components/organisms/navbar/navbar.js
@@ -1,35 +1,72 @@
-import { Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./navbar.module.css";
 import { RxHamburgerMenu } from "react-icons/rx";
-import logo from "../../../image/logo.png"
-import user from "../../../image/man.png"
+import logo from "../../../image/logo.png";
+import user from "../../../image/man.png";
+import { Link } from "react-router-dom";
 
+export function Navbar({ backgrounds, onBackgroundClick }) {
+    
+  const [showBackgroundList, setShowBackgroundList] = useState(false);
+  const [selectedBackgroundIndex, setSelectedBackgroundIndex] = useState(0);
+  
 
+  useEffect(() => {
+    const storedBackgroundIndex = localStorage.getItem("selectedBackgroundIndex");
+    if (storedBackgroundIndex) {
+      setSelectedBackgroundIndex(parseInt(storedBackgroundIndex));
+    }
+  }, []);
 
-export  function Navbar(){
-    return(
-        <Fragment>
-            <div className={styles.navbar}>
+  useEffect(() => {
+    localStorage.setItem("selectedBackgroundIndex", selectedBackgroundIndex.toString());
+    onBackgroundClick(selectedBackgroundIndex);
+  }, [selectedBackgroundIndex, onBackgroundClick]);
 
-              <div className={styles.left}>
-                  <RxHamburgerMenu className={styles.leftLogo}/>
-                  <img className={styles.leftImage} alt="profile" src={logo} height="50px" width="50px"  />
-                   <h1 className={styles.leftH1}>KANBAN</h1>
-              </div>
+ function handleBackgroundClick(i) {
+    setSelectedBackgroundIndex(i);
+  }
 
+ 
 
-   
-   
-              <div className={styles.right}>
-                  
-                    <p className={styles.rightLi}>Home</p>
-                    <p className={styles.rightLi}>Clear Background</p>
-                    <img src={user} 
-                       alt="profile" className={styles.rightImg}  />
-   
-              </div>
-   
-               </div>
-        </Fragment>
-    )
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.left}>
+        <RxHamburgerMenu className={styles.leftLogo} />
+        <img
+          className={styles.leftImage}
+          alt="profile"
+          src={logo}
+          height="50px"
+          width="50px"
+        />
+        <h1 className={styles.leftH1}>KANBAN</h1>
+      </div>
+
+      <div className={styles.right}>
+      <Link to="/" className={styles.link} > <p className={styles.rightLi}>Home</p> </Link>
+        <p className={styles.rightLi} onClick={()=>setShowBackgroundList(!showBackgroundList)}>
+          Change Background
+        </p>
+        <img src={user} alt="profile" className={styles.rightImg} />
+        {showBackgroundList && backgrounds && (
+          <ul className={styles.backgroundList}>
+            {backgrounds.map((background, i) => (
+              <li
+                key={i}
+                className={styles.backgroundItem}
+                onClick={() => handleBackgroundClick(i)}
+              >
+                <img
+                  src={background}
+                  alt="Background"
+                  className={styles.backgroundImage}
+                />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/components/pages/heropage/heroPage.js
+++ src/components/pages/heropage/heroPage.js
@@ -1,41 +1,36 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Navbar } from "../../organisms/navbar/navbar";
 import styles from "./heroPage.module.css";
 import { InitialTask } from "../../molecules/initialTask/initialTask";
 
-export function HeroPage(){
-
+export function HeroPage() {
+  const backgrounds = [
+    "https://images.pexels.com/photos/2034373/pexels-photo-2034373.jpeg?cs=srgb&dl=pexels-romka-%C2%A0-2034373.jpg&fm=jpg",
+    "https://wallpaperaccess.com/full/1391399.png",
+    "https://wallpaperaccess.com/full/4183210.jpg",
+    "https://wallpaperaccess.com/full/3896261.jpg",
+    "https://images.pexels.com/photos/459271/pexels-photo-459271.jpeg?auto=compress&cs=tinysrgb&w=300",
+    "https://media.istockphoto.com/id/1355464383/photo/abstract-composition-with-connecting-dots-and-lines-blue-background-plexus-effect-big-data-3d.webp?b=1&s=170667a&w=0&k=20&c=tBzuzW1yQ4Oj7XHD10LSZ8oTM21nSIYBo3BLo7c2YGM=",
+    "https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2FsbXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+    "https://wallpaperaccess.com/full/4182676.jpg",
     
+  ];
 
-    return (
-        <Fragment>
-            <div className={styles.heroPageMain}  >
-                <Navbar/>
+  const [selectedBackground, setSelectedBackground] = useState(null);
 
-                
+  function onBackgroundClick(i) {
+    setSelectedBackground(backgrounds[i]);
+  }
 
-               
-                <InitialTask/>
-
-              
-
-            
-
-          
-
-
-
-
-
-
-
-
-
-
-
-            </div>
-
-            
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <div
+        className={styles.heroPageMain}
+        style={{ backgroundImage: `url(${selectedBackground})` }}
+      >
+        <Navbar backgrounds={backgrounds} onBackgroundClick={onBackgroundClick} />
+        <InitialTask />
+      </div>
+    </Fragment>
+  );
 }
